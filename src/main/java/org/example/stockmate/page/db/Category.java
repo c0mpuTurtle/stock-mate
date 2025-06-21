c96b1e1420330fc10e17e1450057737774047b52
hyebin <shin378378@naver.com>
2025-06-21 21:18:50 +0900

    Feat : Category 만들기

---
+++ src/main/java/org/example/stockmate/page/db/Category.java
@@ -0,0 +1,29 @@
+package org.example.stockmate.page.db;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "sub_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", columnDefinition = "bigint", nullable = false)
+    private Long id;
+
+    @Column(name = "category_name", columnDefinition = "varchar(50)", nullable = false)
+    private String name;
+
+    @Builder
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
